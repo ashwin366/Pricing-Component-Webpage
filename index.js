9299b417b705cbdde6a6015ceaf9c1a91cc00057
Ashwin <ashwin.ashwin366@gmiail.com>
2020-12-09 03:44:31 +0530

    Add flip to cards

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-
 var toggle = document.querySelector(".switch");
-var price = document.querySelectorAll(".toggle-price");
-var card = document.querySelectorAll(".card");
+var card = document.querySelectorAll(".card-holder");
+var i = 0;
 
-toggle.addEventListener("click", function(){
-  var t = 300;
-  for (var i = 0; i< price.length; i++){
+toggle.addEventListener("click", function() {
+
+  for (i = 0; i < card.length; i++) {
     card[i].classList.toggle("flip");
-    delay(i,t);
-    t = t + 500;
   }
-})
 
-function delay(i,t){
-  setTimeout(function(){
-      // card[i].classList.toggle("flip-x");
-      price[i].classList.toggle("hidden");
-  },t);
-}
+})
